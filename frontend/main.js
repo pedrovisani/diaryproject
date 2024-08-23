a6c73fddb92f7c4d99fc0613b3441bf0ae06f749
Pedro Visani <pedrovisani@K4AC>
2024-08-23 15:58:20 -0300

    inclusões e alterações aulas 159

--- frontend/main.js
+++ frontend/main.js
@@ -1,4 +1,12 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
+import Login from './modules/Login';
+
 import './assets/css/style.css';
+
+const login = new Login('.formLogin');
+const cadastro = new Login('.formCadastro');
+
+login.init();
+cadastro.init();
