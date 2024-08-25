1bab2ec499cc6fb9407bf4b15ea7f70f190a54f6
Pedro Visani <pedrovisani@K4AC>
2024-08-25 02:42:02 -0300

    inclusões e alterações pós aula 159

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'production',
+    mode: 'development',
     entry: './frontend/main.js',
     output: {
         path: path.resolve(__dirname, 'public', 'assets', 'js'),
